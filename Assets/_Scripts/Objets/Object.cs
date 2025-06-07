f0049d7de927f8f551f931dbe8c72102c7e51e9d
Le Canne <94301906+LeCanne@users.noreply.github.com>
2025-06-07 13:33:48 +0200

    CutSystem

---
+++ Assets/_Scripts/Objets/Object.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Splines;
+
+
+public class Object : MonoBehaviour
+{
+    //Graphic References
+    public SpriteRenderer r_spriteRenderer;
+    public SpriteRenderer l_spriteRenderer;
+    public Sprite r_sprite;
+    public Sprite l_sprite;
+    private GameObject r_object;
+    private GameObject l_object;
+    public string objectName;
+
+    //Spline Values
+    public AnimationCurve animationCurve;
+    public float speed;
+    public SplineContainer splinePath;
+    private Spline spline;
+
+    //Spline Parameters
+    public GameObject moveObject;
+    public bool loop;
+    private float currentTime;
+
+
+    //Set Cuttable Time
+    [Range(0f, 1f)] public float Cut;
+    public float reactionTime;
+    bool cutCalled;
+
+    //CutContols
+    InputAction cut;
+
+    //Set cuttable object
+    bool canCut;
+    bool isCut;
+
+   
+
+
+    private void Awake()
+    {
+        r_spriteRenderer.sprite = r_sprite;
+        l_spriteRenderer.sprite = l_sprite; 
+
+        r_object = r_spriteRenderer.gameObject;
+        l_object = l_spriteRenderer.gameObject;
+
+        spline = splinePath.Spline;
+        cut = InputSystem.actions.FindAction("Attack");
+        
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isCut)
+        {
+            TravelSpline();
+        }
+
+        SlashObject();
+        
+
+    }
+
+    public void TravelSpline()
+    {
+       
+        if (loop)
+        {
+            currentTime += Time.deltaTime * speed;
+            //Loop the animation
+            if (currentTime > 1)
+            {
+                currentTime = 0;
+            }
+        }
+        else if (currentTime < 1)
+        {
+            currentTime += Time.deltaTime * speed;
+        }
+        Vector3 currentDistance = spline.EvaluatePosition(animationCurve.Evaluate(currentTime));
+        moveObject.transform.position = currentDistance + splinePath.gameObject.transform.position;
+
+        
+    }
+
+    public virtual void SlashObject()
+    {
+        if (currentTime >= Cut && cutCalled == false)
+        {
+            StartCoroutine(CuttingTime());
+            cutCalled = true;
+
+
+         
+        }
+
+        if (cut.triggered)
+        {
+            if (canCut == true)
+            {
+                Debug.Log(" SUCCESS !");
+                isCut = true;
+                canCut = false;
+                LaunchObjet();
+            }
+            else
+            {
+                Debug.Log(" FAILURE !");
+            }
+           
+        }
+      
+    }
+
+    private void LaunchObjet()
+    {
+       Rigidbody2D r_rb = l_object.GetComponent<Rigidbody2D>();
+       Rigidbody2D l_rb = r_object.GetComponent<Rigidbody2D>();
+
+
+        r_rb.simulated = true;
+        l_rb.simulated = true;
+
+        
+        r_rb.AddForce(r_object.transform.right * 10, ForceMode2D.Impulse);
+        l_rb.AddForce(-l_object.transform.right * 10, ForceMode2D.Impulse);
+    }
+    
+
+    IEnumerator CuttingTime()
+    {
+        Debug.Log("CUT!");
+        canCut = true;
+        yield return new WaitForSeconds(reactionTime);
+        canCut = false;
+        
+    }
+
+    
+}
